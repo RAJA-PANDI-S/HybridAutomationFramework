ef17bb1e181b48f865e729117f28f5bc0fa56e48
RAJA-PANDI-S <rajapandirp2712@gmail.com>
2024-09-21 10:34:40 +0530

    TC1 Working code.

--- src/test/java/org/test/framework/StepDefinitions/StepDefinition.java
+++ src/test/java/org/test/framework/StepDefinitions/StepDefinition.java
@@ -8,16 +8,12 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
+//
 
 import java.time.Instant;
-
 
 
 public class StepDefinition {
@@ -26,16 +22,15 @@ public class StepDefinition {
 
     @Before
     public void setUp() {
-      //  WebDriver driver;
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Raja\\chromedriver-win64\\chromedriver.exe");
+        //  WebDriver driver;
+        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\Raja\\chromedriver-win64\\chromedriver.exe");
         driver = new ChromeDriver();
     }
 
+    @Test
     @Given("User is on google homepage")
     public void user_is_on_google_homepage() {
-       System.out.println("This is Step 1");
-      //  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Raja\\chromedriver-win64\\chromedriver.exe");
-     //   driver = new ChromeDriver();
+        System.out.println("This is Step 1");
         driver.navigate().to("http://www.google.com/");
         driver.manage().window().maximize();
         System.out.println("Google Started at \t" + Instant.now());
@@ -67,70 +62,17 @@ public class StepDefinition {
         System.out.println("Opened Tickertape Site at \t" + Instant.now());
         // Get the title of the page
         String actualTitle = driver.getTitle();
-
         String expectedTitle = "Stock Analysis & Best Financial Tools for Indian Stock Market Evaluation | Tickertape";
-//content="Stock Analysis & Best Financial Tools for Indian Stock Market Evaluation | Tickertape"
-        // Verify the title using TestNG Assert
+        //content="Stock Analysis & Best Financial Tools for Indian Stock Market Evaluation | Tickertape"
         Assert.assertEquals(actualTitle, expectedTitle, "Title does not match the expected value!");
+        System.out.println(actualTitle);
     }
-
 
     @After
     public void browserClose() {
-        driver.close();
+        //driver.close();
         driver.quit();
         System.out.println("Browser closed at \t" + Instant.now());
     }
 }
 
-/*
-Testing started at 23:34 ...
-This is Step 1
-
-Step failed
-java.lang.NullPointerException: Cannot invoke "org.openqa.selenium.WebDriver.navigate()" because "this.driver" is null
-	at org.test.framework.StepDefinitions.StepDefinition.user_is_on_google_homepage(StepDefinition.java:34)
-	at ✽.User is on google homepage(file:///C:/Users/Raja/IntellijProjects/HybridAutomationFramework/src/test/resources/Features/TestScenarios.feature:4)
-
-
-Step skipped
-
-Step skipped
-
-Step skipped
-
-Step skipped
-
-Failed scenarios:
-file:///C:/Users/Raja/IntellijProjects/HybridAutomationFramework/src/test/resources/Features/TestScenarios.feature:3 # User searches Tickertape on Google and navigates to the site
-
-1 Scenarios (1 failed)
-5 Steps (1 failed, 4 skipped)
-0m0.746s
-
-
-java.lang.NullPointerException: Cannot invoke "org.openqa.selenium.WebDriver.navigate()" because "this.driver" is null
-	at org.test.framework.StepDefinitions.StepDefinition.user_is_on_google_homepage(StepDefinition.java:34)
-	at ✽.User is on google homepage(file:///C:/Users/Raja/IntellijProjects/HybridAutomationFramework/src/test/resources/Features/TestScenarios.feature:4)
-
-
-┌───────────────────────────────────────────────────────────────────────────────────┐
-│ Share your Cucumber Report with your team at https://reports.cucumber.io          │
-│ Activate publishing with one of the following:                                    │
-│                                                                                   │
-│ src/test/resources/cucumber.properties:          cucumber.publish.enabled=true    │
-│ src/test/resources/junit-platform.properties:    cucumber.publish.enabled=true    │
-│ Environment variable:                            CUCUMBER_PUBLISH_ENABLED=true    │
-│ JUnit:                                           @CucumberOptions(publish = true) │
-│                                                                                   │
-│ More information at https://reports.cucumber.io/docs/cucumber-jvm                 │
-│                                                                                   │
-│ Disable this message with one of the following:                                   │
-│                                                                                   │
-│ src/test/resources/cucumber.properties:          cucumber.publish.quiet=true      │
-│ src/test/resources/junit-platform.properties:    cucumber.publish.quiet=true      │
-└───────────────────────────────────────────────────────────────────────────────────┘
-
-Process finished with exit code 1
-
- */
