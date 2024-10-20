7a784c6cfe952ae00c341c35077142416f1964bf
RAJA-PANDI-S <rajapandirp2712@gmail.com>
2024-10-20 21:49:19 +0530

    New TC2 added for TradingView.

--- src/test/java/org/test/framework/StepDefinitions/StepsForTradingView.java
+++ src/test/java/org/test/framework/StepDefinitions/StepsForTradingView.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -23,7 +24,7 @@ import java.util.List;
 public class StepsForTradingView {
     WebDriver driver = HooksStepDefinition.driver;
     //WebDriverWait wait;
-    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
 
     @Test
@@ -126,7 +127,7 @@ public class StepsForTradingView {
         // driver.findElement(By.xpath("//span[contains(text(),\"Sign in\")]")).click();
         Thread.sleep(3000);
         String actualTitle = driver.getTitle();
-        TakeSnapshot snapshot = new TakeSnapshot(driver, "TradingView_Homepage");
+        TakeSnapshot snapshot = new TakeSnapshot(driver, "TradingView_Loginpage");
         //String expectedTitle = "";
         //Assert.assertEquals(actualTitle, expectedTitle, "Title does not match the expected value!");
         System.out.println("==========================================================================");
@@ -137,44 +138,46 @@ public class StepsForTradingView {
     }
 
     //============================================================================================
-/*
+
     //TEST CASE 2
-    @Then("Login page appears where User enters invalid Username or Password")
-    public void login_page_appears_where_user_enters_invalid_username_or_password() throws Exception {
 
-        String filePath = "src/main/resources/Config/user_credentials.xlsx";
-        ExcelDataDrivenUtil.loadExcel(filePath);
-
-        // Read the username and password from the Excel file
-        String username = ExcelDataDrivenUtil.getCellData("Sheet1", 1, 0);
-        String password = ExcelDataDrivenUtil.getCellData("Sheet1", 1, 1);
-
-        String encryptedPassword = EncryptionUtil.encrypt(password);
-        System.out.println("Password Encrypted Successfully --> " + encryptedPassword);
-        String decryptedPassword = EncryptionUtil.decrypt(encryptedPassword);
-
-        WebElement UsernameField = driver.findElement(By.xpath("//input[@name='id_username']"));
-        UsernameField.click();
-        UsernameField.sendKeys(username);
-
-        WebElement PasswordField = driver.findElement(By.xpath("//input[@name='id_password']"));
-        PasswordField.click();
-        PasswordField.sendKeys(encryptedPassword);
+    @When("User Clicks on Search bar")
+    public void user_clicks_on_search_bar() throws InterruptedException {
+        driver.findElement(By.xpath("//span[@class='searchText-PCujdK9L']")).click();
+        Thread.sleep(3000);
 
     }
 
-    @Then("login should failed and info message should displayed as {string}")
-    public void login_should_failed_and_info_message_should_displayed_as(String string) throws InterruptedException {
-        driver.findElement(By.xpath("//span[contains(text(),\"Sign in\")]")).click();
-        Thread.sleep(5000);
-        String errText = "Invalid password. If you've forgot your password, try using the Log in with Google button.";
-        String Text = driver.findElement(By.xpath("//div[@class='text-wrap-TCHLKPuQ']")).getText();
-        LoggerUtil.info(Text);
+    @When("Search for a stock and hits enter")
+    public void search_for_a_stock_and_hits_enter() throws InterruptedException {
+        String input = "SBIN";
+        WebElement searchInput = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@class='input-KLRTYDjH' and @type='search']")));
+        for (int i = 0; i < input.length(); i++) {
+            // Extract the character at the current index and convert it to a string.
+            searchInput.sendKeys(String.valueOf(input.charAt(i)));
+            // Add a delay or wait (e.g., for user interface responsiveness).
+            Thread.sleep(2000);
+        }
+        Thread.sleep(3000);
+        searchInput.sendKeys(Keys.ENTER);
     }
-*/
-    //============================================================================================
 
+    @Then("Chart opens with a candlestick pattern showing")
+    public void chart_opens_with_a_candlestick_pattern_showing() {
 
-    //===========================================================================================
+        //button[text()='STATE BANK OF INDIA']
+        WebElement searchInput = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//button[text()='STATE BANK OF INDIA']")));
+
+        String actualTitle = driver.getTitle();
+        TakeSnapshot snapshot = new TakeSnapshot(driver, "TradingView_Chart");
+        //String expectedTitle = "";
+        //Assert.assertEquals(actualTitle, expectedTitle, "Title does not match the expected value!");
+        System.out.println("==========================================================================");
+        System.out.println(actualTitle);
+        System.out.println("==========================================================================");
+        //logger.info("Test 1 completed Successfully");
+        LoggerUtil.info("TradingView TestCase 2 completed Successfully");
+
+    }
 
 }
--- src/test/resources/Features/TradingView.feature
+++ src/test/resources/Features/TradingView.feature
@@ -8,4 +8,9 @@ Feature: Trading View Test Automation Scenarios
     Then Login page appears where User enters Username and Password
     And User clicks sign in button and able to login successfully
 
-
+  @TV_TestCase2 @TV_Regression
+  Scenario: Verify that User a stock and view the chart
+    Given URL to the TradingView site
+    When User Clicks on Search bar
+    And Search for a stock and hits enter
+    Then Chart opens with a candlestick pattern showing
